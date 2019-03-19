5c948ff2dad6d202c6bda5930a9b1930cf7fb710
DiabloZ <diabloz@me.com>
2019-03-19 18:05:38 +0300

    Добавлен функционал автоматически запрашивающий через сайт актуальный список валют.

--- Homework/Homework_1_1.js
+++ Homework/Homework_1_1.js
@@ -2,14 +2,26 @@ $(document).ready(function(){
 	getValue();
 	
 });
+
 function getValue(){
 	$.get(
 		"https://www.cbr-xml-daily.ru/daily_json.js",
 		function(data){
 			data = JSON.parse(data);
 			console.log(data);
-			alert(data['Valute']['USD']['Value']);
+			
+			var Rub = prompt("Введите необходимую сумму - ");
+			var Usd = (data['Valute']['USD']['Value']);
+			var Eur = (data['Valute']['EUR']['Value']);
+			var sumEur = parseFloat((Rub / Eur).toFixed(2));
+			var sumUsd = parseFloat((Rub / Usd).toFixed(2));
+
+			alert ("В пересчёте на евро будет - " + sumEur);			
+			alert ("В пересчёте на доллары будет - " + sumUsd);
+
 	}
 	);
 	
+	
 }
+
